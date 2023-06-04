bc379be0e132303dbe942c9df0886f98227a72ab
francisco rafael <francisco_rafael@hotmail.com.br>
2023-06-03 21:11:48 -0300

    Hands-on 9 - Projeto Pratico 1

--- Projeto_Pratico1/app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ Projeto_Pratico1/app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,13 +8,14 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit{
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova Task", checked: true},
-    { task: "Minha nova Task2", checked: false}
-  ];
+  public taskList: Array<TaskList> = [];
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  public setEmitTaskList(event: string) {
+   this.taskList.push({ task: event, checked: false})
   }
 
   public deleteItemTaskList(event: number) {
